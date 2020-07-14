d8ac8272d99750db36edcdc1fbaa0dfe3d6cb026
Eric Silk <eric.silk@ericsilk.com>
2020-07-13 19:36:11 -0700

    Example thrust working, build fixed.

--- LU_factorization/lu_dense_gpu.cu
+++ LU_factorization/lu_dense_gpu.cu
@@ -4,13 +4,21 @@
 #include <thrust/sort.h>
 #include <thrust/copy.h>
 #include <algorithm>
+#include <vector>
 #include <cstdlib>
+#include <ctime>
 
 int main(void)
 {
   // generate 32M random numbers serially
   thrust::host_vector<int> h_vec(32 << 20);
+  std::srand(std::time(nullptr));
   std::generate(h_vec.begin(), h_vec.end(), rand);
+  std::vector<int> local_vec;
+  local_vec.resize(32 << 20);
+  std::copy(h_vec.begin(), h_vec.end(), local_vec.begin());
+  std::cout << "First:" << local_vec[0] << std::endl;
+  std::cout << "Last:" << local_vec.back() << std::endl;
 
   // transfer data to the device
   thrust::device_vector<int> d_vec = h_vec;
@@ -20,6 +28,10 @@ int main(void)
 
   // transfer data back to host
   thrust::copy(d_vec.begin(), d_vec.end(), h_vec.begin());
+
+  std::copy(h_vec.begin(), h_vec.end(), local_vec.begin());
+  std::cout << "First:" << local_vec[0] << std::endl;
+  std::cout << "Last:" << local_vec.back() << std::endl;
 
   return 0;
 }
