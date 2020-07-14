16caa46cb9a9aa48d66f94f43838cea68ad1060c
Eric Silk <eric.silk@ericsilk.com>
2020-07-13 22:02:19 -0700

    scale and add compiling and running, but wrong. indexing issue most likely

--- LU_factorization/lu_dense_gpu.cu
+++ LU_factorization/lu_dense_gpu.cu
@@ -7,31 +7,125 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <math.h>
+#include <assert.h>
 
-int main(void)
+struct LU
 {
-  // generate 32M random numbers serially
-  thrust::host_vector<int> h_vec(32 << 20);
+  LU() = default;
+  std::vector<double> L;
+  std::vector<double> U;
+  size_t n;
+};
+
+// Borrowed from the transformations example code
+// https://docs.nvidia.com/cuda/thrust/index.html#transformations
+struct daxpy_functor
+{
+  const double a;
+  daxpy_functor(double _a) : a(_a) {}
+  __host__ __device__
+    double operator()(const double& x, const double&y) const
+    {
+      return a * x + y;
+    }
+};
+
+void daxpy(double A, thrust::device_vector<double>& X, thrust::device_vector<double>& Y)
+{
+  thrust::transform(X.begin(), X.end(), Y.begin(), Y.begin(), daxpy_functor(A));
+}
+
+LU LU_factorization(const std::vector<double>& A, size_t n);
+void print_matrix(const std::vector<double>& A, size_t n);
+double rand_0_1(void);
+
+int main(int argc, char **argv)
+{
+  int n = 0;
+  if (argc >= 2)
+  {
+    n = atoi(argv[1]);
+  }
+  else
+  {
+    n = 1024;
+  }
+
+  std::vector<double> A;
+  A.resize(n*n);
   std::srand(std::time(nullptr));
-  std::generate(h_vec.begin(), h_vec.end(), rand);
-  std::vector<int> local_vec;
-  local_vec.resize(32 << 20);
-  std::copy(h_vec.begin(), h_vec.end(), local_vec.begin());
-  std::cout << "First:" << local_vec[0] << std::endl;
-  std::cout << "Last:" << local_vec.back() << std::endl;
+  std::generate(A.begin(), A.end(), rand_0_1);
+  std::cout << "A:" << std::endl;
+  print_matrix(A, n);
 
-  // transfer data to the device
-  thrust::device_vector<int> d_vec = h_vec;
+  auto factored = LU_factorization(A, n);
 
-  // sort data on the device (846M keys per second on GeForce GTX 480)
-  thrust::sort(d_vec.begin(), d_vec.end());
-
-  // transfer data back to host
-  thrust::copy(d_vec.begin(), d_vec.end(), h_vec.begin());
-
-  std::copy(h_vec.begin(), h_vec.end(), local_vec.begin());
-  std::cout << "First:" << local_vec[0] << std::endl;
-  std::cout << "Last:" << local_vec.back() << std::endl;
+  std::cout << "U:" << std::endl;
+  print_matrix(factored.U, n);
+  std::cout << "L:" << std::endl;
+  print_matrix(factored.L, n);
 
   return 0;
+}
+double rand_0_1(void)
+{
+  return ((double) rand() / (RAND_MAX));
+}
+
+void print_matrix(const std::vector<double>& A, size_t n)
+{
+  for (size_t row = 0; row < n; ++row)
+  {
+    for (size_t col = 0; col < n; ++col)
+    {
+      std::cout << A[n*row+col] << " ";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+}
+
+LU LU_factorization(const std::vector<double>& A, const size_t n)
+{
+  assert(A.size() == n*n);
+  std::vector<double> U(n*n);
+  std::vector<double> L(n*n, 0);
+
+  // Initialize them
+  std::copy(A.begin(), A.end(), U.begin());
+  for (size_t i = 0; i < n; ++i)
+  {
+    L[i*(n+1)] = 1.0;
+  }
+
+  thrust::host_vector<double> top_row_host(n);
+  thrust::device_vector<double> top_row_dev(n);
+  thrust::host_vector<double> reducing_row_host(n);
+  thrust::device_vector<double> reducing_row_dev(n);
+
+  for (size_t col = 0; col < n-1; ++col)
+  {
+    std::copy(A.begin()+(col*n), A.begin()+((col+1)*n), top_row_host.begin());
+    top_row_dev = top_row_host;
+    for (int row = col+1; row < n; ++row)
+    {
+      double coeff = -(A[row*n+col] / A[col*n+col]);
+      // Copy the Rows to the host vector, then device vector
+      std::copy(A.begin()+(row*n), A.begin()+(2*row*n), reducing_row_host.begin());
+      reducing_row_dev = reducing_row_host;
+      // Scale and add
+      daxpy(coeff, top_row_dev, reducing_row_dev);
+      reducing_row_host = reducing_row_dev;
+      thrust::copy(reducing_row_host.begin(),
+                   reducing_row_host.end(),
+                   U.begin()+(row*n));
+    }
+  }
+
+  LU retval;
+  retval.U = U;
+  retval.L = L;
+
+  return retval;
 }
