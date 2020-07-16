2b9fc2ea1890f0dae93ac4df0c76ce3a655ae375
Eric Silk <eric.silk@ericsilk.com>
2020-07-15 17:52:03 -0700

    Copied a strided iterator from the Thrust examples

---
+++ LU_factorization/strided_iterator.h
@@ -0,0 +1,61 @@
+// Copied and adapted from:
+// https://github.com/thrust/thrust/blob/master/examples/strided_range.cu
+#include <thrust/iterator/counting_iterator.h>
+#include <thrust/iterator/transform_iterator.h>
+#include <thrust/iterator/permutation_iterator.h>
+#include <thrust/functional.h>
+#include <thrust/fill.h>
+#include <thrust/device_vector.h>
+#include <thrust/copy.h>
+
+template <typename Iterator>
+class strided_range
+{
+  public:
+    typedef typename thrust::iterator_difference<Iterator>::type difference_type;
+
+    struct stride_functor : public thrust::unary_function<difference_type, difference_type>
+    {
+      difference_type stride;
+      stride_functor(difference_type stride)
+        : stride(stride)
+      {
+        // NTD
+      }
+
+      __host__ __device__
+      difference_type operator()(const difference_type& i) const
+      {
+        return stride * i;
+      }
+    };
+
+    typedef typename thrust::counting_iterator<difference_type>                   CountingIterator;
+    typedef typename thrust::transform_iterator<stride_functor, CountingIterator> TransformIterator;
+    typedef typename thrust::permutation_iterator<Iterator, TransformIterator>    PermutationIterator;
+
+    typedef PermutationIterator iterator;
+
+    strided_range(Iterator first, Iterator last, difference_type stride)
+      : first(first)
+      , last(last)
+      , stride(stride)
+    {
+      // NTD
+    }
+
+    iterator begin(void) const
+    {
+      return PermutationIterator(first, TransformIterator(CountingIterator(0), stride_functor(stride));
+    }
+
+    iterator end(void) const
+    {
+      return begin() + ((last - first) + (stride - 1)) / stride;
+    }
+  
+  protected:
+    Iterator first;
+    Iterator last;
+    difference_type stride;
+};
