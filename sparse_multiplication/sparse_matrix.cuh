2533aff6927134188b42eae58875b1a3bc334cc8
Eric Silk <eric.silk@ericsilk.com>
2020-07-22 06:53:32 -0700

    Got a basic sparse matrix class written

--- sparse_multiplication/sparse_matrix.cuh
+++ sparse_multiplication/sparse_matrix.cuh
@@ -10,51 +10,32 @@
 #include <cstdlib>
 #include <math.h>
 #include <assert.h>
-template<typename T, typename Enable = void>
-class base_ThrustCSRMatrix;
 
-template<typename T>
-class base_ThrustCSRMatrix<T, std::enable_if<std::is_same<T, thrust::device_vector<double>>::value>>
-{
-  private:
-    thrust::device_vector<size_t, thrust::device_allocator<size_t>> m_row_indices, m_col_indices;
-    thrust::device_vector<double, thrust::device_allocator<double>> m_storage;
-};
-
-template<typename T>
-class base_ThrustCSRMatrix<T, std::enable_if<std::is_same<T, thrust::host_vector<double>>::value>>
-{
-  private:
-    thrust::host_vector<size_t, std::allocator<size_t>> m_row_indices, m_col_indices;
-    thrust::host_vector<double, std::allocator<double>> m_storage;
-};
-
-template<class T>
-class ThrustCSRMatrix : private base_ThrustCSRMatrix<T>
+class HostCSRMatrix
 {
   public:
-    ThrustCSRMatrix(size_t rows, size_t cols)
+    HostCSRMatrix(size_t rows, size_t cols)
       : m_is_open(false)
       , m_num_rows(rows)
       , m_num_cols(cols)
-      , m_row_indices(m_num_rows + 1)
+      , m_row_indices(m_num_rows + 1, 0)
     {
       // NTD
     }
-    ThrustCSRMatrix& operator=(const ThrustCSRMatrix& other)
+
+    HostCSRMatrix(size_t rows,
+                  size_t cols,
+                  thrust::host_vector<size_t> row_indices,
+                  thrust::host_vector<size_t> col_indices,
+                  thrust::host_vector<double> storage)
+      : m_is_open(false)
+      , m_num_rows(rows)
+      , m_num_cols(rows)
+      , m_row_indices(row_indices)
+      , m_col_indices(col_indices)
+      , m_storage(storage)
     {
-      if (&other == this)
-      {
-        return *this;
-      }
-      this->m_is_open = other.m_is_open;
-      this->m_num_rows = other.m_num_rows;
-      this->m_num_cols = other.m_num_cols;
-      this->m_row_indices = other.m_row_indices;
-      this->m_col_indices = other.m_col_indices;
-      this->m_storage = other.m_storage;
-      
-      return *this;
+      // NTD
     }
 
     void open_for_pushback(void)
@@ -100,9 +81,118 @@ class ThrustCSRMatrix : private base_ThrustCSRMatrix<T>
     size_t num_cols(void) const { return m_num_cols; };
     size_t num_nonzeros(void) const { return m_storage.size(); };
 
+    thrust::host_vector<size_t> get_row_indices(void) const { return m_row_indices; };
+    thrust::host_vector<size_t> get_col_indices(void) const { return m_col_indices; };
+    thrust::host_vector<double> get_storage(void) const { return m_storage; };
+
   private:
     bool m_is_open;
     size_t m_num_rows, m_num_cols;
+    thrust::host_vector<size_t> m_row_indices, m_col_indices;
+    thrust::host_vector<double> m_storage;
 };
+
+class DeviceCSRMatrix
+{
+  public:
+    DeviceCSRMatrix(size_t rows, size_t cols)
+      : m_is_open(false)
+      , m_num_rows(rows)
+      , m_num_cols(cols)
+      , m_row_indices(m_num_rows + 1, 0)
+    {
+      // NTD
+    }
+
+    DeviceCSRMatrix(size_t rows,
+                    size_t cols,
+                    thrust::device_vector<size_t> row_indices,
+                    thrust::device_vector<size_t> col_indices,
+                    thrust::device_vector<double> storage)
+      : m_is_open(false)
+      , m_num_rows(rows)
+      , m_num_cols(cols)
+      , m_row_indices(row_indices)
+      , m_col_indices(col_indices)
+      , m_storage(storage)
+    {
+      // NTD
+    }
+
+    void open_for_pushback(void)
+    {
+      m_is_open = true;
+    }
+
+    void close_for_pushback(void)
+    {
+      // TODO Thrust-ize these ops?
+      m_is_open = false;
+      for (size_t i = 0; i < m_num_rows; ++i)
+      {
+        m_row_indices[i+1] += m_row_indices[i];
+      }
+      for (size_t i = m_num_rows; i > 0; --i)
+      {
+        m_row_indices[i] = m_row_indices[i-1];
+      }
+      m_row_indices[0] = 0;
+    }
+    
+    void push_back(size_t row, size_t col, double value)
+    {
+       assert(m_is_open);
+       assert(row < m_num_rows);
+       assert(col < m_num_rows);
+
+       // TODO thrust-ize?
+       ++m_row_indices[row];
+       m_col_indices.push_back(col);
+       m_storage.push_back(value);
+    }
+
+    void clear(void)
+    {
+      m_col_indices.clear();
+      m_storage.clear();
+      thrust::fill(m_row_indices.begin(), m_row_indices.end(), 0);
+    }
+
+    size_t num_rows(void) const { return m_num_rows; };
+    size_t num_cols(void) const { return m_num_cols; };
+    size_t num_nonzeros(void) const { return m_storage.size(); };
+
+    thrust::device_vector<size_t> get_row_indices(void) const { return m_row_indices; };
+    thrust::device_vector<size_t> get_col_indices(void) const { return m_col_indices; };
+    thrust::device_vector<double> get_storage(void) const { return m_storage; };
+
+  private:
+    bool m_is_open;
+    size_t m_num_rows, m_num_cols;
+    thrust::device_vector<size_t> m_row_indices, m_col_indices;
+    thrust::device_vector<double> m_storage;
+};
+
+// Copy constructors produced a circular dependece I MIGHT be able to get around
+// using a base class and inheritance, but meh.
+// Templates proved obnoxious.
+// We're going C style, baby.
+HostCSRMatrix dev_to_host(const DeviceCSRMatrix& dev)
+{
+  return HostCSRMatrix(dev.num_rows(),
+                       dev.num_cols(),
+                       dev.get_row_indices(),
+                       dev.get_col_indices(),
+                       dev.get_storage());
+}
+
+DeviceCSRMatrix host_to_dev(const HostCSRMatrix& host)
+{
+  return DeviceCSRMatrix(host.num_rows(),
+                         host.num_cols(),
+                         host.get_row_indices(),
+                         host.get_col_indices(),
+                         host.get_storage());
+}
 
 #endif//HOST_SPARSE_MATRIX_CUH
--- sparse_multiplication/test_sparse_matrix.cu
+++ sparse_multiplication/test_sparse_matrix.cu
@@ -16,13 +16,13 @@ int main(int argc, char** argv)
     n = 6;
   }
 
-  ThrustCSRMatrix<thrust::host_vector> host_csr(n, n);
-  ThrustCSRMatrix<thrust::device_vector> dev_csr(n, n);
+  HostCSRMatrix host_csr(n, n);
   
   // Generate N random numbers and n i,j coords
   std::random_device rd;
   std::mt19937 gen(rd());
-  std::uniform_int_distribution<size_t> coord_distr(0, n);
+  // The distribution is inclusive, hence the n-1
+  std::uniform_int_distribution<size_t> coord_distr(0, n-1);
   const double double_min = std::numeric_limits<double>::min();
   const double double_max = std::numeric_limits<double>::max();
   std::uniform_real_distribution<double> val_distr(double_min, double_max);
@@ -36,4 +36,6 @@ int main(int argc, char** argv)
     host_csr.push_back(row, col, value);
   }
   host_csr.close_for_pushback();
+
+  auto dev_csr = host_to_dev(host_csr);
 }
