01edd1910fb9608d20e4eb7b1dc2e0d071ba12c8
eric.silk@ericsilk.com <eric_silk@selinc.com>
2020-11-05 11:40:18 -0800

    extra printouts, still debugging

--- LU_factorization/qr_dense_cpu.h
+++ LU_factorization/qr_dense_cpu.h
@@ -27,6 +27,7 @@ void QR_col_op(Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& A, size_t col_i
   Eigen::Matrix<T, Eigen::Dynamic, 1> e = Eigen::Matrix<T, Eigen::Dynamic, 1>::Zero(A.rows()-col_i, 1);
   e(0) = 1;
   auto y = A.block(col_i, col_i, A.rows()-col_i, 1);
+  std::cout << "y:" << std::endl << y << std::endl;
   std::cout << "y dims: " << y.rows() << "x" << y.cols() << std::endl;
   std::cout << "e dims: " << e.rows() << "x" << e.cols() << std::endl;
 
@@ -44,6 +45,7 @@ void QR_col_op(Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& A, size_t col_i
   Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> new_y = (I - two_vk_tkt) * y;
 
   std::cout << "And assigning..." << std::endl;
+  std::cout << new_y << std::endl;
   y = new_y;
 }
 
