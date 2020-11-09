f496b8679eac325918f603d8ffa447b2007d9eb3
eric.silk@ericsilk.com <eric_silk@selinc.com>
2020-11-09 13:32:12 -0800

    Wikipedia example working!

--- LU_factorization/qr_dense_cpu.cpp
+++ LU_factorization/qr_dense_cpu.cpp
@@ -2,23 +2,22 @@
 #include <Eigen/Dense>
 #include "qr_dense_cpu.h"
 
-bool wiki_example(void);
+void wiki_example(void);
 
 int main(int argc, char *argv[])
 {
   std::cout << "Starting." << std::endl;
-  bool rslt = wiki_example();
-  std::cout << "Wiki worked: " << rslt << std::endl;
+  wiki_example();
 
   return 0;
 }
 
-bool wiki_example(void)
+void wiki_example(void)
 {
   // https://en.wikipedia.org/wiki/QR_decomposition#Example_2
   Eigen::MatrixXd A = (Eigen::Matrix3d() << 12, -51, 4, 6, 167, -68, -4, 24, -41).finished();
   std::cout << "A:" << std::endl << A << std::endl;
   auto qr_result = QR(A);
-  std::cout << "Result: " << std::endl << qr_result.Q << std::endl;
-  return true;
+  std::cout << "Result, Q: " << std::endl << qr_result.Q << std::endl;
+  std::cout << "Result, R: " << std::endl << qr_result.R << std::endl;
 }
--- LU_factorization/qr_dense_cpu.h
+++ LU_factorization/qr_dense_cpu.h
@@ -7,6 +7,12 @@
 #include <string>
 #include <Eigen/Dense>
 
+template <typename T>
+using Matrix = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
+
+template <typename T>
+using Vector = Eigen::Matrix<T, Eigen::Dynamic, 1>;
+
 enum QRTechnique
 {
   Householder,
@@ -16,50 +22,56 @@ enum QRTechnique
 template<typename T=double>
 struct QRMatrices
 {
-
-  Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> Q;
-  Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> R;
+  QRMatrices(Matrix<T> Q_, Matrix<T> R_)
+    : Q(Q_)
+    , R(R_)
+  {
+    // NTD
+  }
+  QRMatrices(void)
+    : Q()
+    , R()
+  {
+    // NTD
+  }
+  Matrix<T> Q;
+  Matrix<T> R;
 };
 
 template<typename T=double>
-void QR_col_op(Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& A, size_t col_i)
+void QR_col_op(const Matrix<T>& A, Matrix<T>& Q, Matrix<T>& R, size_t col_i)
 {
-  Eigen::Matrix<T, Eigen::Dynamic, 1> e = Eigen::Matrix<T, Eigen::Dynamic, 1>::Zero(A.rows()-col_i, 1);
+  Vector<T> e = Vector<T>::Zero(A.rows()-col_i, 1);
   e(0) = 1;
-  auto y = A.block(col_i, col_i, A.rows()-col_i, 1);
-  std::cout << "y:" << std::endl << y << std::endl;
-  std::cout << "y dims: " << y.rows() << "x" << y.cols() << std::endl;
-  std::cout << "e dims: " << e.rows() << "x" << e.cols() << std::endl;
+  auto x = R.block(col_i, col_i, A.rows()-col_i, 1);
 
-  T sign_y = (y(0, 0) >= 0 ? 1 : -1);
-  auto w = y - sign_y * y.norm() * e;
-  std::cout << "w dims: " << w.rows() << "x" << w.cols() << std::endl;
-  auto v = w.normalized();
+  T sign_x = (A(col_i, col_i) >= 0 ? 1 : -1);
+  T alpha = x.norm();
+  auto u = x - sign_x * alpha * e;
+  auto v = u.normalized();
   auto two_vk_tkt = 2*v*v.transpose();
-  std::cout << "two_vk_tkt dims: " << two_vk_tkt.rows() << "x" << two_vk_tkt.cols() << std::endl;
   size_t I_rows = two_vk_tkt.rows();
   size_t I_cols = two_vk_tkt.cols();
 
-  auto I = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>::Identity(I_rows, I_cols);
-  std::cout << "I dims: " << I.rows() << "x" << I.cols() << std::endl;
-  Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> new_y = (I - two_vk_tkt) * y;
-
-  std::cout << "And assigning..." << std::endl;
-  std::cout << new_y << std::endl;
-  y = new_y;
+  Matrix<T> I = Matrix<T>::Identity(I_rows, I_cols);
+  Matrix<T> Q_i = Matrix<T>::Identity(A.rows(), A.cols());
+  Q_i.block(col_i, col_i, A.rows()-col_i, A.cols()-col_i) = (I - two_vk_tkt);
+  R = Q_i*R;
+  Q = Q * Q_i.transpose();
 }
 
 template<typename T=double>
 QRMatrices<T> QR(Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& A)
 {
-  for (size_t col = 0; col < A.cols(); ++col)
+  Matrix<T> R = A;
+  Matrix<T> Q = Matrix<T>::Identity(A.rows(), A.cols());
+  for (size_t col = 0; col < A.cols()-1; ++col)
   {
-    std::cout << "col_i: " << col << std::endl;
-    QR_col_op(A, col);
+    QR_col_op(A, Q, R, col);
   }
 
-  QRMatrices<T> retval;
-  retval.Q = A;
+  QRMatrices<T> retval(Q, R);
+  return retval;
 }
 
 #endif//QR_DENSE_CPU_H
